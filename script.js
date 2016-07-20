3a947825eee58e1da306bcbc22f400b6bc0c6bbe
Scott Humphries <git@sscotth.io>
2016-07-20 13:51:06 -0500

    hidden class semi-spa

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('Welcome to our website fellow developer!')
+
+var navLinks = document.querySelectorAll('li')
+var pages = document.querySelectorAll('.page')
+
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', clickedNavLink);
+}
+
+function clickedNavLink (evt) {
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].classList.add('hidden')
+  }
+
+  document.querySelector(evt.target.hash).classList.remove('hidden')
+}
