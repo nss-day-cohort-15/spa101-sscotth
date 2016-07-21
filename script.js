c21b2ae108c61752eb0ea83523df8702708e9d3f
Scott Humphries <git@sscotth.io>
2016-07-21 11:16:40 -0500

    convert to iife

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 console.log('Welcome to our website fellow developer!')
 
-var navLinks = document.querySelectorAll('li')
-var pages = document.querySelectorAll('.page')
+(function () {
+  var navLinks = document.querySelectorAll('li')
+  var pages = document.querySelectorAll('.page')
 
-showPage(location.hash)
+  showPage(location.hash)
 
-for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener('click', clickedNavLink);
-}
-
-function clickedNavLink (evt) {
-  for (var i = 0; i < pages.length; i++) {
-    pages[i].classList.add('hidden')
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', clickedNavLink);
   }
 
-  showPage(evt.target.hash)
-}
+  function clickedNavLink (evt) {
+    for (var i = 0; i < pages.length; i++) {
+      pages[i].classList.add('hidden')
+    }
 
-function showPage (id) {
-  if (id) {
-    document.querySelector(id).classList.remove('hidden')
-  } else {
-    document.querySelector('#home').classList.remove('hidden')
+    showPage(evt.target.hash)
   }
-}
+
+  function showPage (id) {
+    if (id) {
+      document.querySelector(id).classList.remove('hidden')
+    } else {
+      document.querySelector('#home').classList.remove('hidden')
+    }
+  }
+
+}())
