23857758a2cbccf98febb5055953be542abb16fb
Scott Humphries <git@sscotth.io>
2016-07-21 11:59:49 -0500

    nested iife

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 console.log('Welcome to our website fellow developer!')
 
 (function () {
-  var navLinks = document.querySelectorAll('li')
-  var pages = document.querySelectorAll('.page')
 
   showPage(location.hash)
 
-  for (var i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener('click', clickedNavLink);
-  }
+  // You can nest IIFES for additional segmentation
+  // and also name IIFEs for additional context
+  (function addNavLinkEvents () {
+    var navLinks = document.querySelectorAll('li')
+
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].addEventListener('click', clickedNavLink);
+    }
+  }())
 
   function clickedNavLink (evt) {
+    var pages = document.querySelectorAll('.page')
+
     for (var i = 0; i < pages.length; i++) {
       pages[i].classList.add('hidden')
     }
